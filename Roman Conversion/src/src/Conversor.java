6cd736c72d9d9028e33787db91632d2da2994fd1
Luiza Schaidt <luiza.schaidt@gmail.com>
2014-05-22 14:02:12 -0300

    iniciacao do projeto Roman Conversion

---
+++ Roman Conversion/src/src/Conversor.java
@@ -0,0 +1,19 @@
+package src;
+
+public class Conversor {
+
+	public int romanToArabic(String roman) {
+		
+		String newRoman=null;
+		
+		for(int i=roman.length(); i>0; i--){
+			
+			System.out.println(roman.charAt(i-1));
+		
+		}
+		return 22;
+	}
+
+	
+	
+}
---
+++ Roman Conversion/src/tests/test.java
@@ -0,0 +1,38 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.Test;
+
+import src.Conversor;
+
+public class test {
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+	
+	Conversor conv ;
+	String roman;
+	int arabic;
+
+	@Before
+	public void setUp() throws Exception {
+		conv = new Conversor();
+		roman = "XXII";
+		arabic = 22;
+	}
+
+	@Test
+	public void testRomanToArabic() {
+		assertEquals(arabic,conv.romanToArabic(roman));
+	}
+
+}
